e4c991888b4636993a3c197649b3fa8dd9f6447b
Diana0604 <diana.vallverdu@gmail.com>
2020-01-15 10:14:05 +0000

    questions

--- app.js
+++ app.js
@@ -1,55 +1,102 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+    app = express(),
+     bodyParser = require('body-parser'),
+     mongoose = require('mongoose');
+
 app.use(express.static("public"));
-var bodyParser = require('body-parser');
 
-function startApp(){
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.set("view engine", "ejs");
 
-    app.get("/", function(req, res){
-        res.render("index");
-    });
-    app.get("/test", function(req, res){
-        res.render("index");
-    });
-    app.get("/test/finish", function(req, res){
-        res.render("finish");
-    });
-    app.get("/test/:question", function(req, res){
-        if(isNaN(req.params.question)){
-            res.send("this page is not valid");
-        } else{
-            var questionNumber = Number(req.params.question) - 1;
-            questions = [
-                {
-                    title: "Question 1",
-                    question: "What do you think about this test?",
-                    questionType: "text"
-                }
-            ];
-            if(questionNumber < questions.length){
-                res.render("test", {question:questions[questionNumber]});
-            } else{
-                res.redirect("/test/finish");
-            }
+
+mongoose.connect("mongodb://localhost/valentine", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+
+//schema setup
+var questiomSchema = new mongoose.Schema({
+    title: String, 
+    question: String,
+    questionType: String,
+    number: Number
+});
+var Question = mongoose.model("Question", questiomSchema);
+/*
+Question.create(
+    {
+        title: "Question 1",
+        question: "What do you think about this test?",
+        questionType: "text",
+        number: 1
+    }, function(err, question){
+        if(err){
+            console.log("Error: " + err);
+        } else {
+            console.log("neq question added: ");
+            console.log(question);
         }
-    });
-    app.post("/:answer", function(req, res){
-        console.log(req.params.answer);
-        var nextQ = Number(req.params.answer) + 1;
-        console.log(req.body.answer);
-        console.log("nest question: " + nextQ);
-        res.redirect("test/" + nextQ);
-    });
-    app.get("*", function(req, res){
-        res.send('this page does not exists');
-    });
+    }
+);
 
-    app.listen(3000, function(){
-        console.log('server started');
-        
-    });
-}
+Question.create(
+    {
+        title: "Question 2",
+        question: "Are you ready to find true love?",
+        questionType: "text",
+        number: 2
+    }, function(err, question){
+        if(err){
+            console.log("Error: " + err);
+        } else {
+            console.log("neq question added: ");
+            console.log(question);
+        }
+    }
+);*/
 
-startApp();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+app.get("/test", function(req, res){
+    res.render("index");
+});
+app.get("/test/finish", function(req, res){
+    res.render("finish");
+});
+app.get("/test/:question", function(req, res){
+    if(isNaN(req.params.question)){
+        res.send("this page is not valid");
+    } else{
+        var questionNumber = Number(req.params.question) - 1;
+        //Get questions from db
+        Question.find({}, function(err, questions){
+            if(err){
+                console.log("ERROR! " + err);
+            } else{
+                if(questionNumber < questions.length){
+                    res.render("test", {question:questions[questionNumber]});
+                } else{
+                    res.redirect("/test/finish");
+                }
+            }
+        });
+    }
+});
+app.post("/:answer", function(req, res){
+    console.log(req.params.answer);
+    var nextQ = Number(req.params.answer) + 1;
+    console.log(req.body.answer);
+    console.log("nest question: " + nextQ);
+    res.redirect("test/" + nextQ);
+});
+app.get("*", function(req, res){
+    res.send('this page does not exists');
+});
+
+app.listen(3000, function(){
+    console.log('server started');
+    
+});
