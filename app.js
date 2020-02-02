347f6d1b44a6e3bab6c9c635d73d0148df797136
Diana0604 <diana.vallverdu@gmail.com>
2020-02-02 16:00:17 +0000

    profile routes added to app

--- app.js
+++ app.js
@@ -55,19 +55,15 @@ app.get("/", function(req, res){
 app.use(questionsRoutes);
 app.use(testRoutes);
 app.use(authRoutes);
+app.use(profileRoutes);
 
 //GENERIC:
 app.get("*", function(req, res){
     res.send('this page does not exists');
 });
 //PORT:
+//app.listen(3000, function(){
 app.listen(process.env.PORT, process.env.IP, function(){
     //seedDB();
     console.log('server started');
 });
-/*
-app.listen(3000, function(){
-    //seedDB();
-    console.log('server started');
-});
-*/
