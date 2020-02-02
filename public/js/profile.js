0273fe7271ed8c94086cc3227bb0c407447a70b6
Diana0604 <diana.vallverdu@gmail.com>
2020-02-02 14:03:08 +0000

    edit username buttons

---
+++ public/js/profile.js
@@ -0,0 +1,32 @@
+function initialize(){
+    //general
+    $(".btn-edit").hide();
+}
+
+initialize();
+//username
+$(".edit-icon-username").click(function(){
+    $(".username").prop('disabled', false);
+    $(".edit-username").show();
+    $(".edit-icon-username").hide();
+});
+
+$(".edit-username").click(function(){
+    $(".email").prop('disabled', true);
+    $(".btn-edit").hide();
+    $(".edit-icon-email").show();
+    $(".edit-icon-username").show();
+});
+//email
+$(".edit-icon-email").click(function(){
+    $(".email").prop('disabled', false);
+    $(".edit-email").show();
+    $(".edit-icon-email").hide();
+});
+
+$(".edit-email").click(function(){
+    $(".email").prop('disabled', true);
+    $(".btn-edit").hide();
+    $(".edit-icon-email").show();
+    $(".edit-icon-username").show();
+});
