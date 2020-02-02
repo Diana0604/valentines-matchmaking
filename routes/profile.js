bb41605791c5b288eff60783f670774a14cb4839
Diana0604 <diana.vallverdu@gmail.com>
2020-02-02 15:38:16 +0000

    edit profile

--- routes/profile.js
+++ routes/profile.js
@@ -5,8 +5,26 @@ var express = require('express'),
 
 router.get('/profile',isLoggedIn, function(req,res) {
     User.findById(req.user._id, function(err, user){
-        console.log('showing profile of user: ' + user);
         res.render('profile', {user:req.user});
+    });
+});
+
+router.post('/profile', isLoggedIn, function(req, res){
+    console.log(req.body);
+    var newUser = req.user;
+    if(req.body.email){
+        newUser.email = req.body.email;
+        console.log('email changed to: ' + newUser.email);
+    }
+    if(req.body.username){
+        newUser = req.body.username;
+        console.log('username changed to: ' + newUser.username);
+    }
+    console.log('new user will be: ' + newUser);
+    User.findByIdAndUpdate(newUser._id, newUser, function(err, updatedUser){
+        console.log('updated user:');
+        console.log(updatedUser);
+        res.redirect('/profile');
     });
 });
 
