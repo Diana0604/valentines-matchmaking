1640a8f9531e9a24a5e83b90c9a338c44458f485
Diana0604 <diana.vallverdu@gmail.com>
2020-02-13 11:36:09 +0000

    matchmaking has been done

--- routes/profile.js
+++ routes/profile.js
@@ -1,3 +1,35 @@
+var matchings = new Map();
+matchings.set("Diana", "SarahShear");
+matchings.set("SarahShear", "SarahShear");
+matchings.set("Henry", "GeorgieKirk");
+matchings.set("GeorgieKirk", "Henry");
+matchings.set("FunnyGirl", "Three kids stacked up on each other in a trenchcoat");
+matchings.set("Three kids stacked up on each other in a trenchcoat", "FunnyGirl");
+matchings.set("Neta CT", "@amymawer");
+matchings.set("@amymawer", "Neta CT");
+matchings.set("OmarZat", "Nathalie");
+matchings.set("Nathalie", "OmarZat");
+matchings.set("TaviaFox", "BW");
+matchings.set("BW", "TaviaFox");
+matchings.set("Eva", "Mengqiu");
+matchings.set("Mengqiu", "Eva");
+matchings.set("Zhaolin", "amaliapasxal@outlook.com");
+matchings.set("amaliapasxal@outlook.com", "Zhaolin");
+matchings.set("Lululizard", "Juliet");
+matchings.set("Juliet", "Lululizard");
+matchings.set("Danny Romeo", "Paul H. Barnes");
+matchings.set("Paul H. Barnes", "Danny Romeo");
+matchings.set("loverboy2000", "Clo");
+matchings.set("Clo", "loverboy2000");
+matchings.set("club_kant_even_handle_me", "Avgi");
+matchings.set("Avgi", "club_kant_even_handle_me");
+matchings.set("Maddie", "inespecas");
+matchings.set("inespecas", "Maddie");
+matchings.set("Natasha", "Yuan");
+matchings.set("Yuan", "Natasha");
+matchings.set("mandajoensson", "Qiyue Luo");
+matchings.set("Qiyue Luo", "mandajoensson");
+
 var express = require('express'), 
     router = express.Router(),
     User = require('../models/user'),
@@ -5,8 +37,11 @@ var express = require('express'),
 
 router.get('/profile',isLoggedIn, function(req,res) {
     User.findById(req.user._id, function(err, user){
-        res.render('profile', {user:req.user});
-    });
+        User.findOne({'username':matchings.get(user.username)}, function(err, match){
+            res.render('profile', {user:req.user, match:match.username, email:match.email});
+        })});
+        //res.render('profile', {user:req.user, match:matchings.get(user.username)});
+    //});
 });
 
 router.post('/profile', isLoggedIn, function(req, res){
